52c8ca96eff8f79b31b7dfd27b08a2eaf5735a65
Rex Ye <rxye1991@gmail.com>
2020-04-19 15:44:19 -0400

    createdCoversationContainerComponent

---
+++ src/Components/ConversationContainer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ConversationContainer extends Component {
+    render() {
+        return (
+            <div className='conver-container'>
+                
+            </div>
+        )
+    }
+}
+
+export default ConversationContainer
--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import {withRouter} from 'react-router'
 
+import ConversationContainer from './ConversationContainer'
+
 export class Homepage extends Component {
     render() {
         const {user} = this.props
@@ -9,6 +11,7 @@ export class Homepage extends Component {
         return (
             <div>
                 <h4>Logged In as: {username}</h4>
+                <h5>Users on the app:</h5>
             </div>
         )
     }
