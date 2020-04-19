802cf6c756ffb290edacac85e77f9da39c9944cf
Rex Ye <rxye1991@gmail.com>
2020-04-19 13:03:07 -0400

    createdFormtosendmessage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import ChatContainer from './Components/ChatContainer'
 export class App extends Component {
   render() {
     return (
-      <div>
-        
+      <div >
+          <h3 id ='app-title'> Chatting App</h3>
+          <ChatContainer/>
       </div>
     )
   }
---
+++ src/Components/ChatContainer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class ChatContainer extends Component {
+    render() {
+        return (
+            <div className = 'chat-container'>
+                        <h1>sdfsdf</h1>
+                        <form action="">
+                            <textarea type="text" cols='70' rows='3' placeholder ='message'/>
+                            <button type='submit'>Send</button>
+                        </form>
+            </div>
+        )
+    }
+}
+
+export default ChatContainer
